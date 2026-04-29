cebff72054b946490eb62ec9655b57db13d039b4
Saad Shahd <dev.saad.shahd@gmail.com>
2026-04-29 10:51:21 +0100

    feat(overlay): wire PortalContext into shadow root for react-aria overlays

--- packages/editor/src/editor/overlay/index.ts
+++ packages/editor/src/editor/overlay/index.ts
@@ -3,3 +3,4 @@ export { useShadowSheet } from "./use-shadow-sheet.js";
 export { useRegistryAnchor } from "./use-registry-anchor.js";
 export { useOnClickOutside } from "./use-on-click-outside.js";
 export { isEditable } from "./dom-utils.js";
+export { PortalContext } from "./portal-context.js";
